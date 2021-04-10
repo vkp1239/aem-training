1d7b8d3a7fb780cb45c17053a256dc90b2f0090c
vkp1239 <81604927+vkp1239@users.noreply.github.com>
2021-04-10 12:24:30 +0530

    Osgi Configurations created for system user and resource resolver

--- com.aem.training/core/src/main/java/training/core/servlets/MyCustomServlet.java
+++ com.aem.training/core/src/main/java/training/core/servlets/MyCustomServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.Servlet;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.framework.Constants;
@@ -14,33 +15,36 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import training.core.services.CustomService;
+import training.core.util.ResolverUtil;
 
-@Component(service=Servlet.class,
-property={
-        Constants.SERVICE_DESCRIPTION + "=Simple custom Servlet",
-        "sling.servlet.methods=" + HttpConstants.METHOD_GET,
-        "sling.servlet.resourceTypes="+ "training/components/structure/page",
-        "sling.servlet.selectors=" + "select"
-        //"sling.servlet.extensions=" + "txt"
-       
+@Component(service = Servlet.class, property = { Constants.SERVICE_DESCRIPTION + "=Simple custom Servlet",
+		"sling.servlet.methods=" + HttpConstants.METHOD_GET,
+		"sling.servlet.resourceTypes=" + "training/components/structure/page", "sling.servlet.selectors=" + "select"
+// "sling.servlet.extensions=" + "txt"
+
 })
-public class MyCustomServlet extends SlingSafeMethodsServlet{
-	
+public class MyCustomServlet extends SlingSafeMethodsServlet {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Reference
 	CustomService customService;
 
-	@Override
-    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
-		
+	@Reference
+	ResourceResolverFactory resourceResolverFactory;
 
-		response.getWriter().write("There goes   servlet response -->" + customService.userName());
-		//response.getWriter().write("There goes   servlet response" );
+	@Override
+	protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException,
+			IOException {
+
+		String message = "   Hello System User :-- ";
+		message = message + ResolverUtil.getResourceResolver(resourceResolverFactory).getUserID();
+
+		response.getWriter().write("There goes   servlet response -->" + customService.userName() + message);
+		// response.getWriter().write("There goes   servlet response" );
 	}
-         
 
 }
